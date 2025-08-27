6e44e7c8639add14441b4c9e2a836e1982b710c5
Ciel-gtz <w613ccg@gmail.com>
2025-08-27 16:25:03 -0400

    Finalizados CalculoCuadrado y HolaMundo

--- Perez_Ciel_HolaMundo.cpp
+++ Perez_Ciel_HolaMundo.cpp
@@ -1,1 +1,8 @@
-//test
+#include <iostream> // Incluye las declaraciones de las facilidades de E/S (entrada/salida) estandar
+using namespace std; // Para no escribir std:: siempre
+
+int main(){ // El programa C++ minimo
+    cout << "Hola Mundo!\n";
+
+    return 0; // Un valor distinto a 0 de main() indica fallo
+}
