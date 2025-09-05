b1910b7a4b24d3c00f6a6b237b5a1a79e55c2622
Ciel-gtz <w613ccg@gmail.com>
2025-09-05 15:37:56 -0400

    Borrando archivos

--- Perez_Ciel_Ejercicio_2.cpp
+++ Perez_Ciel_Ejercicio_2.cpp
@@ -9,23 +9,36 @@ struct Paciente {
     int altura;
 };
 
+// Nodo de la lista enlazada
+struct Nodo {
+    Paciente datos;
+    Nodo* siguiente;
+};
 
 
-// Funciones: Imprimir, Agregar y Eliminar pacientes
-void imprimirPaciente(Paciente paciente) { 
+    // Funciones: Imprimir, Agregar y Eliminar pacientes
+/*void imprimirPaciente(Paciente paciente) { 
     cout << "\n" << "> Nombre: " << paciente.nombre;
     cout << "\n" << "> Edad: " << paciente.edad;
     cout << "\n" << "> Peso: " << paciente.peso << " kg";
     cout << "\n" << "> Altura: " << paciente.altura << " cm";
     cout << "\n__________________";
+}*/
+
+void agregarPaciente(Nodo*& head, const Paciente& paciente) {
+    Nodo* nuevo = new Nodo{paciente, nullptr};
+    if (head == nullptr) {
+        head = nuevo;
+    } else {
+        Nodo* temp = head;
+        while (temp->siguiente != nullptr) {
+            temp = temp->siguiente;
+        }
+        temp->siguiente = nuevo;
+    }
 }
 
-void agregarPaciente(Paciente pacientes[], int &numPacientes, Paciente nuevoPaciente) {///***
-    pacientes[numPacientes] = nuevoPaciente;
-    numPacientes++;
-}
-
-void eliminarPaciente(Paciente pacientes[], int &numPacientes, int indice) {///***
+/*void eliminarPaciente(Paciente pacientes[], int &numPacientes, int indice) {///***
     if (indice < 0 || indice >= numPacientes) {
         cout << "Indice invalido." << endl;
         return;
@@ -34,28 +47,30 @@ void eliminarPaciente(Paciente pacientes[], int &numPacientes, int indice) {///*
         pacientes[i] = pacientes[i + 1];
     }
     numPacientes--;
-}
-
+}*/
 
 
 // Funcion principal
-int main(){ 
-
-    Paciente pacientes[3]; // Lista dinamica de pacientes*******
+int main(){     
+    Nodo* ptrPaciente = nullptr; // Puntero a paciente vacio
 
     // Ejemplos de pacientes
     Paciente paciente1 = {"Cuca", 33, 74.5, 135};
     Paciente paciente2 = {"Gertrudis", 21, 60.0, 165};
     Paciente paciente3 = {"ElefanteGuerreroPsiquicoAncestral", 100, 140.7, 200};
 
-    // Almacena los pacientes en el arreglo**** Cambiar esto por lista dinamica iggggg lolz
-    pacientes[0] = paciente1;
-    pacientes[1] = paciente2;
-    pacientes[2] = paciente3;
+    // Almacena los pacientes en la lista dinamica por medio de la funcion agregarPaciente
+    agregarPaciente(ptrPaciente, paciente1);
+    agregarPaciente(ptrPaciente, paciente2);
+    agregarPaciente(ptrPaciente, paciente3);
 
-    imprimirPaciente(pacientes[0]); // Imprime paciente 1
+    /*pacientes.push_back(paciente1);
+    pacientes.push_back(paciente2);
+    pacientes.push_back(paciente3);*/
+
+    /*imprimirPaciente(pacientes[0]); // Imprime paciente 1
     imprimirPaciente(pacientes[1]); // Imprime paciente 2
-    imprimirPaciente(pacientes[2]); // Imprime paciente 3
+    imprimirPaciente(pacientes[2]); // Imprime paciente 3*/
 
     return 0; 
 }
